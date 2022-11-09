791cf70cfcec95c9d462262a81580c28c5c84c4c
TaraRoopram <59647464+TaraRoopram@users.noreply.github.com>
2022-11-09 10:43:40 +0100

    steps 1, 2, 3

--- SAT_solver.py
+++ SAT_solver.py
@@ -1,27 +1,40 @@
 # Helpful links
 # https://github.com/muneeb706/sudoku-solver/blob/master/basic_sudoku_solver/sudoku-solver.py
 # https://users.aalto.fi/~tjunttil/2020-DP-AUT/notes-sat/solving.html
-# https://github.com/mxklabs/mxklabs-python/tree/master/mxklabs/dimacs
 # https://github.com/DRTooley/PythonSatSolver --> implements three different algorithms incl. dpll
 # https://github.com/marcmelis/dpll-sat/blob/master/solvers/base_sat.py
 
 ## three different strategies: the DPLL algorithm without any further heuristics and two different heuristics
 
 #1: Read DIMACS input
-clauses = []
-with open("sudoku1.cnf",'r') as file:
-    next(file) #skips first row in file
-    for line in file:
-        data = line.split()
-        clause = [int(x) for x in line[:-2].split()]
-        clauses.append(clause)
-    print(clauses) # prints clauses as list of lists
+def read_dimacs_input(filename):
+    clauses = []
+    with open(filename,'r') as file:
+        next(file) #skips first row in file
+        for line in file:
+            data = line.split()
+            clause = [int(x) for x in line[:-2].split()]
+            clauses.append(clause)
+        return clauses # prints clauses as list of lists
 
 #2: Encode Sudoku rules as clauses in DIMACS
-
+read_dimacs_input("sudoku-rules-4x4.txt") # reads sudoku-rules-4x4.txt; to print add print() statement
 
 #3: Encode a given puzzle in DIMACS
-
+read_dimacs_input("sudoku1.cnf") # reads sudoku1.cnf; to print add print() statement
 
 #4: Give (2)+(3) as input to (1) and return the solution to the given puzzle
 
+## Pseudocode DPLL --> https://github.com/safwankdb/SAT-Solver-using-DPLL
+# solve_dpll(cnf):
+#     while(cnf has a unit clause {X}):
+#         delete clauses contatining {X}
+#         delete {!X} from all clauses
+#     if null clause exists:
+#         return False
+#     if CNF is null:
+#         return True
+#     select a literal {X}
+#     cnf1 = cnf + {X}
+#     cnf2 = cnf + {!X}
+#     return solve_dpll(cnf1)+solve_dpll(cnf2)
